1123b76106185df8a80c8f810b4547666e17d2e5
deni1688 <denisj@in-motus.com>
2018-04-16 00:00:07 +0200

    finished inital API endpoints

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
@@ -6,8 +7,20 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const bc = new Blockchain();
 
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+// initial API endpoints
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
+});
+
+app.post('/mine', (req, res) => {
+    const block = bc.addBlock(req.body.data);
+    console.log(`new block added: ${block.toString()}`);
+    res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => console.log(`listening on port ${HTTP_PORT}`));
